041af0478b9b1bee4428035499a0e12379f9b8c3
TziviS-9010210 <tzivi0534@gmail.com>
2025-01-01 15:22:04 +0200

    Update1 index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+console.log("lea you are the best!!")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
